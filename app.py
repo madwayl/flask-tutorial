2b06efc912f3c4efbb22d568850d8e52b1fdc32f
Mahmood Athil <mahmoodathil@WINDELLAP-1096.local>
2024-12-31 22:00:04 +0530

    update app with DB Job Model from MySQL & SQLAlchemy

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask, render_template, jsonify
+from sql.persistobject import session
+from sql.model import Jobs
 
 app = Flask(__name__)
 
-JOBS = [
-    { 'id': 1, 'title': 'Frontend Engineer', 'location': 'Remote', 'salary': 'Rs. 22,00,000' },
-    { 'id': 2, 'title': 'Backtend Engineer', 'location': 'Remote', 'salary': 'Rs. 22,00,000' },
-    { 'id': 3, 'title': 'Devend Engineer', 'location': 'Remote', 'salary': 'Rs. 15,00,000' },
-    { 'id': 3, 'title': 'IT Engineer', 'location': 'In-Office, San Francisco', 'salary': 'Rs. 13,00,000' },
-]
+# JOBS = [
+#     { 'id': 1, 'title': 'Frontend Engineer', 'location': 'Remote', 'salary': 'Rs. 22,00,000' },
+#     { 'id': 2, 'title': 'Backtend Engineer', 'location': 'Remote', 'salary': 'Rs. 22,00,000' },
+#     { 'id': 3, 'title': 'Devend Engineer', 'location': 'Remote', 'salary': 'Rs. 15,00,000' },
+#     { 'id': 3, 'title': 'IT Engineer', 'location': 'In-Office, San Francisco', 'salary': 'Rs. 13,00,000' },
+# ]
+
+def getJobsFromTable():
+    return list(session.query(Jobs))
 
 @app.route("/")
 def index():
+    JOBS = getJobsFromTable()
+    print(JOBS)
     return render_template('home.html', name='Careers', jobs=JOBS)
 
-@app.route("/api/jobs")
-def list_jobs():
-    return jsonify(JOBS)
+# @app.route("/api/jobs")
+# def list_jobs():
+#     return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
